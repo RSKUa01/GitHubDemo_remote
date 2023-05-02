dd893fb46c390c77fa0518900ae2677b04bbbc70
Oleksandr Bilous <bilous.rsk@gmail.com>
2023-05-02 20:43:26 +0300

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Олександр Білоус on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
